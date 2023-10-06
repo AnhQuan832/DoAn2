f7afd64f36aec43fd4ac07a4a45113eacd97113d
Anh Quan <quandang123654@gmail.com>
2023-10-06 13:59:44 +0700

    generate login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticationComponent } from './pages/authentication/authentication.component';
-import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthenticationComponent,
-    AuthLayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/auth-layout/auth-layout-routing.module.ts
+++ src/app/layout/auth-layout/auth-layout-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthenticationComponent } from 'src/app/pages/authentication/authentication.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: AuthenticationComponent
+    component: LoginComponent
   }
 ];
 
